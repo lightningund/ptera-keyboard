095bf98eaa01141c5ead8e9f2f6dcddd3ca61a27
lightningund <lightningundies0826@gmail.com>
2024-07-25 12:42:24 -0500

    updated project definition

--- project.clj
+++ project.clj
@@ -1,11 +1,16 @@
-(defproject ptera "0.1.0-SNAPSHOT"
-  :description "A parametrized, split-hand, concave, columnar, erogonomic keyboard"
-  :url "http://example.com/FIXME"
-  :main ptera
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [unicode-math "0.2.0"]
-                 [scad-clj "0.4.0"]])
-
-
+(defproject ptera "0.1.0"
+	:description "A parametrized, split-hand, concave, columnar, erogonomic keyboard"
+	:url "https://github.com/lightningund/ptera-keyboard/tree/master"
+	:main ptera
+	:license {
+		:name "GNU AGPLv3"
+		:url "https://www.gnu.org/licenses/agpl-3.0.en.html#license-text"}
+	:dependencies [
+		[org.clojure/clojure "1.7.0"]
+		[unicode-math "0.2.0"]
+		[scad-clj "0.4.0"]
+	]
+	:cljfmt {
+		:indentation? false
+	}
+)
