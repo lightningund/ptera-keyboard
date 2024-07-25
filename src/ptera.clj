56e1c3f1a67dab135cba266fc7b03e5badc2b35d
lightningund <lightningundies0826@gmail.com>
2024-07-25 17:05:51 -0500

    reorganizing

--- src/ptera.clj
+++ src/ptera.clj
@@ -1,11 +1,16 @@
 (ns ptera
 	(:refer-clojure :exclude [use import])
-	(:require [clojure.core.matrix :refer [array matrix mmul]]
-						[scad-clj.scad :refer :all]
-						[scad-clj.model :refer :all]
-						[unicode-math.core :refer :all]))
+	(:require
+		[clojure.core.matrix :refer [array matrix mmul]]
+		[scad-clj.scad :refer :all]
+		[scad-clj.model :refer :all]
+		[unicode-math.core :refer :all]
+	)
+)
 
 (defn deg2rad [degrees] (* (/ degrees 180) pi))
+
+(defn half [num] (/ num 2))
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; Shape parameters ;;
@@ -21,10 +26,13 @@
 (def tenting-angle (/ π 4)) ; or, change this for more precise tenting control
 (def column-style (if (> nrows 5) :orthographic :standard)) ; options include :standard, :orthographic
 
-(defn column-offset [column] (cond
-	(= column 2) [0 2.82 -4.5]
-	(>= column 4) [0 -12 5.64] ; original [0 -5.8 5.64]
-	:else [0 0 0]))
+(defn column-offset [column]
+	(cond
+		(= column 2) [0 2.82 -4.5]
+		(>= column 4) [0 -12 5.64] ; original [0 -5.8 5.64]
+		:else [0 0 0]
+	)
+)
 
 (def thumb-offsets [6 -3 -6])
 
@@ -36,16 +44,6 @@
 (def wall-z-offset -15) ; length of the first downward-sloping part of the wall (negative)
 (def wall-xy-offset 8) ; offset in the x and/or y direction for the first downward-sloping part of the wall (negative)
 (def wall-thickness 2) ; wall thickness parameter; originally 5
-
-;; Settings for column-style == :fixed
-;; The defaults roughly match Maltron settings
-;; http://patentimages.storage.googleapis.com/EP0219944A2/imgf0002.png
-;; Fixed-z overrides the z portion of the column ofsets above.
-;; NOTE: THIS DOESN'T WORK QUITE LIKE I'D HOPED.
-(def fixed-angles [(deg2rad 10) (deg2rad 10) 0 0 0 (deg2rad -15) (deg2rad -15)])
-(def fixed-x [-41.5 -22.5 0 20.3 41.4 65.5 89.6]) ; relative to the middle finger
-(def fixed-z [12.1 8.3 0 5 10.7 14.5 17.5])
-(def fixed-tenting (deg2rad 0))
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;; General variables ;;
@@ -67,8 +65,6 @@
 (def plate-thickness 4)
 (def mount-width (+ keyswitch-width 3))
 (def mount-height (+ keyswitch-height 3))
-
-(defn half [num] (/ num 2))
 
 (def single-plate
 	(let [
@@ -214,6 +210,13 @@
 			(translate-fn [(- (* (- column centercol) column-x-delta)) 0 column-z-delta])
 			(translate-fn (column-offset column))
 		)
+		; Settings for column-style == :fixed
+		; The defaults roughly match Maltron settings
+		; Fixed-z overrides the z portion of the column offsets
+		fixed-angles [(deg2rad 10) (deg2rad 10) 0 0 0 (deg2rad -15) (deg2rad -15)]
+		fixed-x [-41.5 -22.5 0 20.3 41.4 65.5 89.6] ; relative to the middle finger
+		fixed-z [12.1 8.3 0 5 10.7 14.5 17.5]
+		fixed-tenting (deg2rad 0)
 		placed-shape-fixed (->>
 			shape
 			(rotate-y-fn (nth fixed-angles column))
